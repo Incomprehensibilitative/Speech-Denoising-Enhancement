bfbdf1af33e04dcc2de704afbdb7b0fbf376dd1e
Incomprehensibilitative <dangmh.bi12-076@st.usth.edu.vn>
2023-06-13 15:30:48 +0700

    test in a separate file

--- techniques/wiener/wiener_filtering.py
+++ techniques/wiener/wiener_filtering.py
@@ -65,19 +65,4 @@ def apply_wiener_filter(input_file, output_file):
     plt.show()
 
 
-# List all WAV files in the folder
-input_files = '../../audio_files/audio/airport.wav', '../../audio_files/audio/cafe.wav', '../../audio_files/audio' \
-                                                                                         '/rainstorm.wav'
-# Specify the output folder path
-output_dir = '../../filtered_audio_files/'
 
-# Process each input file
-for input_file in input_files:
-    # Extract the file name from the input_file path
-    name_of_file = input_file.split('/')[-1]
-
-    # Create the output file path with the desired prefix and directory
-    output_file = f"{output_dir}wf_{name_of_file}"
-
-    # Apply Wiener filter and save the filtered audio
-    apply_wiener_filter(input_file, output_file)
--- techniques/wiener_test.py
+++ techniques/wiener_test.py
@@ -1,4 +1,17 @@
-import numpy as np 
-from utils import audio_reader as ar
+from wiener.wiener_filtering import apply_wiener_filter
+# List all WAV files in the folder
+input_files = '../../audio_files/audio/airport.wav', '../../audio_files/audio/cafe.wav', '../../audio_files/audio' \
+                                                                                         '/rainstorm.wav'
+# Specify the output folder path
+output_dir = '../../filtered_audio_files/'
 
-print("hello")
+# Process each input file
+for input_file in input_files:
+    # Extract the file name from the input_file path
+    name_of_file = input_file.split('/')[-1]
+
+    # Create the output file path with the desired prefix and directory
+    output_file = f"{output_dir}wf_{name_of_file}"
+
+    # Apply Wiener filter and save the filtered audio
+    apply_wiener_filter(input_file, output_file)
